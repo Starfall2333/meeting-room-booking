c76aa88e76dd4e3689ce67a31728b9a9cace1cde
starfall2333 <starfall2333@outlook.com>
2026-07-05 19:34:06 +0800

    feat: 完成 Sprint 2 核心功能MVP - 实现 JWT 登录/注册，配置 Security 拦截规则 - 实现会议室后端 CRUD 接口 - 前端封装 Axios，实现登录页和会议室管理页

--- backend/src/main/java/com/campus/meeting/controller/HelloController.java
+++ backend/src/main/java/com/campus/meeting/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.campus.meeting.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(String name) {
         System.out.println("HelloController …… hello : " + name);
         return "hello" + name;
